e26a4d253dac043c36cd80ce46d78f9794655a13
Brandon Medina <49995667+sh1royasha@users.noreply.github.com>
2024-06-02 02:02:58 -0600

    Update cloudinaryConfig.js

--- server/config/cloudinaryConfig.js
+++ server/config/cloudinaryConfig.js
@@ -1,9 +1,10 @@
 import {v2 as cloudinary} from 'cloudinary';
-          
+import {CLOUDINARY_NAME, CLOUDINARY_API_KEY , CLOUDINARY_API_SECRET  } from './config.js'
+
 cloudinary.config({ 
-  cloud_name: 'ddc7x3iyc', 
-  api_key: '348193454752765', 
-  api_secret: 'Zx4CJ1GBfFUxGaedEnJ9-VDzPqQ' 
+  cloud_name: CLOUDINARY_NAME, 
+  api_key: CLOUDINARY_API_KEY, 
+  api_secret: CLOUDINARY_API_SECRET
 });
 
 export default cloudinary;
